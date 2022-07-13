7f59e534745881f50f09d7c343fa5cad83d0d9a4
Varis Rana <vrvarisrana123@gmail.com>
2022-07-13 06:19:34 +0530

    commit 13

---
+++ RangeSumQueryImmutable.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RangeSumQueryImmutable {
+      int[] prefix;
+
+      public void NumArray(int[] nums) {
+          int n=nums.length;
+          prefix=new int[n];
+          prefix[0]=nums[0];
+          for(int i=1; i<n; i++){
+              prefix[i]=nums[i]+prefix[i-1];
+          }
+      }
+
+      public int sumRange(int left, int right) {
+            if (left == 0)
+                  return prefix[right];
+            return prefix[right] - prefix[left - 1];
+      }
+}
